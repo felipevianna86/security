801651fa56e177eda4ab015f8ebbdca213105bea
felipevianna <felipevianna@unirn.edu.br>
2020-07-12 11:31:35 -0300

    Autenticação em memória.

---
+++ src/test/java/br/com/security/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.security.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
